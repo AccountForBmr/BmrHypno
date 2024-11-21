2ea6ba2199b992eec71302718348a026210d52c2
AccountForBmr <pierinoangela2000@gmail.com>
2024-11-22 00:42:32 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -7,6 +7,7 @@ var startBmr=function(){
   elm.id="mainBox";
   elm.style="position:absolute;z-index:100;top:0;left:0;width:50%;height:50%;background-color:blue;";
   GUI.instance.DisplayMessage("Random change to check if jsdelivr updates");
+  ACTION_BAR.instance.TriggerMacro("","/s AHHHHHHHHHHHHHHHHHHHHHH It's working!!!!");
   return elm;
 }
 
