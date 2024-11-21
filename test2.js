a751b0cee965943c5e05c15dbe3165fb85f1837a
Dhmis <pierinoangela2000@gmail.com>
2024-11-21 17:50:09 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,3 +1,10 @@
 //a comment, ahhhhhhhhhhhh!
 console.log("AAAAAAAHHHH");
 var abcd1234={"A":12,"B":34};
+
+var startBmr=function(){
+  var elm=document.createElement("div");
+  elm.id="mainBox";
+  elm.style="position:absolute;z-index:100;top:0;left:0;width:50%;height:50%;background-color:blue;";
+  return elm;
+}
