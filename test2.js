f4ccef86471b7b20cf20ff679955faac3bfd765e
Dhmis <pierinoangela2000@gmail.com>
2024-11-21 23:53:58 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -8,3 +8,5 @@ var startBmr=function(){
   elm.style="position:absolute;z-index:100;top:0;left:0;width:50%;height:50%;background-color:blue;";
   return elm;
 }
+
+GUI.instance.DisplayMessage("Test");
