3c47d963eb35e93ab8c6e766ef3a5c5a90526e10
AccountForBmr <pierinoangela2000@gmail.com>
2024-11-22 01:17:37 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -5,9 +5,11 @@ var abcd1234={"A":12,"B":34};
 var startBmr=function(){
   var elm=document.createElement("div");
   elm.id="mainBox";
-  elm.style="position:absolute;z-index:100;top:0;left:0;width:50%;height:50%;background-color:blue;";
+  elm.style="position:relative;z-index:100;top:10%;left:25%;width:50%;height:80%;background:rgba(0, 0, 0, 0.9);color:#fff;border:2px solid #343434;"+
+  "border-radius:0.5em;padding:0.25em;font-size:calc(var(--scale) * 2);backdrop-filter:blur(0.2em);pointer-events:auto;"+
+  "box-shadow:inset 0px 0px 30px 2px rgba(0, 0, 0, 0.5), 0px 0px 3px 0px rgba(0, 0, 0, 0.75)";
   GUI.instance.DisplayMessage("Random change to check if jsdelivr updates");
-  ACTION_BAR.instance.TriggerMacro("","/s AHHHHHHHHHHHHHHHHHHHHHH It's working!!!!");
+  ACTION_BAR.TriggerMacro("","/s AHHHHHHHHHHHHHHHHHHHHHH It's working!!!!");
   return elm;
 }
 
