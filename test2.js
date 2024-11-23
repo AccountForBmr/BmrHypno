11a62a5c5398ae0e36c694e1d296bf3bdd7db021
AccountForBmr <pierinoangela2000@gmail.com>
2024-11-23 01:04:42 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -13,7 +13,7 @@ var startBmr = function() {
   mainBox.appendChild(createBmrStartingGrid());
 
   //replacing the dropdown menu in Bmr to add Hypno to the list
-  var menufunc=temp0.onclick.toString().substring(8).slice(0,-1);
+  var menufunc=document.getElementById("menu").getElementsByClassName("button")[0].onclick.toString().substring(8).slice(0,-1);
   menufunc=menufunc.replace(/"Exit/gm,"\"Hypno\", onclick: () => document.getElementById(\"menus\").appendChild(startBmr())},{label:\"Exit");
   menufunc=menufunc.replace(/_menuButton/gm,"document.getElementById(\"menu\").getElementsByClassName(\"button\")[0]");
   document.getElementById("menu").getElementsByClassName("button")[0].onclick=new Function("e",menufunc);
